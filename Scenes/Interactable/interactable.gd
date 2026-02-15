1bcaa1b01c575a06caf9a637af423b50f1bdcd8b
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-02-15 21:40:56 +0200

    some test objects

--- Scenes/Interactable/interactable.gd
+++ Scenes/Interactable/interactable.gd
@@ -4,6 +4,9 @@ signal interacted
 
 @onready var indicator: Sprite2D = $Indicator
 
+func _ready() -> void:
+	indicator.visible = false
+
 # show or hide the interaction indicator
 func set_can_interact(can_interact: bool) -> void:
 	if indicator:
