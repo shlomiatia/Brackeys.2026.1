9feaa40a8e59272d5c619c0c55a8dce5877ec463
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-02-16 21:06:48 +0200

    cp

--- Scenes/Level1/Ball/Ball.gd
+++ Scenes/Level1/Ball/Ball.gd
@@ -2,8 +2,11 @@ class_name Ball extends Node2D
 
 @onready var ball: Sprite2D = $Ball
 @onready var shadow: Sprite2D = $Shadow
-@onready var conversation: Conversation = $Shadow/Conversation
+@onready var conversation: Conversation = $Conversation
 @onready var shaking_camera: ShakingCamera = $"../../ShakingCamera"
+@onready var destroy_floor: AnimatedSprite2D = $DestroyFloor
+@onready var hole: Sprite2D = $Hole
+@onready var hole_collision: CollisionShape2D = $Hole/StaticBody2D/CollisionShape2D
 
 func fall() -> void:
     shaking_camera.start_screen_shake()
@@ -15,7 +18,31 @@ func fall() -> void:
 
     var duration = 0.5
     tween.tween_property(ball, "position:y", -32, duration)
-    tween.tween_property(shadow, "scale", Vector2.ONE, duration)
+    tween.tween_property(shadow, "scale", Vector2(0.8, 0.8), duration)
 
     await tween.finished
     conversation.interactable_disabled = false
+
+func cut() -> void:
+    var tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_IN)
+
+    var duration = 0.5
+    tween.tween_property(ball, "position:y", 0, duration)
+    tween.tween_property(shadow, "scale", Vector2.ONE, duration)
+
+    await tween.finished
+
+    shaking_camera.start_screen_shake()
+
+    ball.visible = false
+    shadow.visible = false
+
+    destroy_floor.play()
+
+    hole.visible = true
+    hole_collision.disabled = false
+
+    conversation.dialogue_name = "level1_hole"
