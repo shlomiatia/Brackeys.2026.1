e3fab006b67e37f405aca8814aecf475fbcd08bb
amethyscent <amethyscent12@gmail.com>
2026-02-16 12:32:55 -0500

    Added in assets for the intro sequence

--- Scenes/Level1/Ball/Ball.gd
+++ Scenes/Level1/Ball/Ball.gd
@@ -5,14 +5,14 @@ extends Node2D
 @onready var conversation: Conversation = $Shadow/Conversation
 
 func fall() -> void:
-    var tween = create_tween()
-    tween.set_parallel(true)
-    tween.set_trans(Tween.TRANS_SINE)
-    tween.set_ease(Tween.EASE_OUT)
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_OUT)
 
-    var duration = 0.5
-    tween.tween_property(ball, "position:y", -32, duration)
-    tween.tween_property(shadow, "scale", Vector2.ONE, duration)
+	var duration = 0.5
+	tween.tween_property(ball, "position:y", -32, duration)
+	tween.tween_property(shadow, "scale", Vector2.ONE, duration)
 
-    await tween.finished
-    conversation.interactable_disabled = false
+	await tween.finished
+	conversation.interactable_disabled = false
