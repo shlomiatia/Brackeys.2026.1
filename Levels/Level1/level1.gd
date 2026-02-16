a091df15428d32204490ab881e1801535fc918be
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-02-16 16:14:34 +0200

    fix bug

--- Levels/Level1/level1.gd
+++ Levels/Level1/level1.gd
@@ -10,47 +10,50 @@ var door_puzzle_solved: bool = false
 @onready var real_door_conversation: Conversation = $Objects/RealDoor/Conversation
 
 func _ready() -> void:
-	# Connect to Dialogic signals
-	Dialogic.signal_event.connect(_on_dialogic_signal)
+    Global.player_can_move = false
+    Global.player_can_interact = false
 
-	# Start sequence: disable player movement and show opening dialog
-	await _show_start_sequence()
+    # Connect to Dialogic signals
+    Dialogic.signal_event.connect(_on_dialogic_signal)
 
-	# Set initial dialogue names for conversations
-	_update_conversation_dialogues()
+    # Start sequence: disable player movement and show opening dialog
+    await _show_start_sequence()
+
+    # Set initial dialogue names for conversations
+    _update_conversation_dialogues()
 
 func _show_start_sequence() -> void:
-	# Wait 1 second
-	await get_tree().create_timer(1.0).timeout
+    # Wait 1 second
+    await get_tree().create_timer(1.0).timeout
 
-	# Show opening dialog
-	await DialogDisplayer.start("start_dialog")
+    # Show opening dialog
+    await DialogDisplayer.start("start_dialog")
 
 func _update_conversation_dialogues() -> void:
-	# Update Door conversation based on puzzle state
-	if not door_puzzle_solved:
-		door_conversation.dialogue_name = "door_locked_sound"
-	else:
-		door_conversation.dialogue_name = "door_locked"
+    # Update Door conversation based on puzzle state
+    if not door_puzzle_solved:
+        door_conversation.dialogue_name = "door_locked_sound"
+    else:
+        door_conversation.dialogue_name = "door_locked"
 
-	# Update RealDoor conversation based on puzzle state
-	if not door_puzzle_solved:
-		real_door_conversation.dialogue_name = "coin_dialog"
-	else:
-		real_door_conversation.dialogue_name = "solved_dialog"
+    # Update RealDoor conversation based on puzzle state
+    if not door_puzzle_solved:
+        real_door_conversation.dialogue_name = "coin_dialog"
+    else:
+        real_door_conversation.dialogue_name = "solved_dialog"
 
 func _on_dialogic_signal(argument: String) -> void:
-	match argument:
-		"solve_puzzle":
-			_solve_door_puzzle()
+    match argument:
+        "solve_puzzle":
+            _solve_door_puzzle()
 
 func _solve_door_puzzle() -> void:
-	# Mark puzzle as solved
-	door_puzzle_solved = true
+    # Mark puzzle as solved
+    door_puzzle_solved = true
 
-	# Change RealDoor texture to tax.png
-	var tax_texture = load("res://Assets/tax.png")
-	real_door.texture = tax_texture
+    # Change RealDoor texture to tax.png
+    var tax_texture = load("res://Assets/tax.png")
+    real_door.texture = tax_texture
 
-	# Update the conversation dialogues for new state
-	_update_conversation_dialogues()
+    # Update the conversation dialogues for new state
+    _update_conversation_dialogues()
