79c88b2512979d93e6a91fb3c4cf1a63e54e5b98
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-02-16 18:38:57 +0200

    basic ball interaction

--- Levels/Level1/level1.gd
+++ Levels/Level1/level1.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 # State tracking
-var door_puzzle_solved: bool = false
+var door_interacted: bool = false
 
 # Node references
 @onready var door: Sprite2D = $Objects/Door
-@onready var real_door: Sprite2D = $Objects/RealDoor
 @onready var door_conversation: Conversation = $Objects/Door/Conversation
-@onready var real_door_conversation: Conversation = $Objects/RealDoor/Conversation
+@onready var ball = $Objects/Door/Ball
 
 func _ready() -> void:
     Global.player_can_move = false
@@ -30,30 +29,15 @@ func _show_start_sequence() -> void:
     await DialogDisplayer.start("start_dialog")
 
 func _update_conversation_dialogues() -> void:
-    # Update Door conversation based on puzzle state
-    if not door_puzzle_solved:
+    # Update Door conversation based on interaction state
+    if not door_interacted:
         door_conversation.dialogue_name = "door_locked_sound"
     else:
         door_conversation.dialogue_name = "door_locked"
 
-    # Update RealDoor conversation based on puzzle state
-    if not door_puzzle_solved:
-        real_door_conversation.dialogue_name = "coin_dialog"
-    else:
-        real_door_conversation.dialogue_name = "solved_dialog"
-
 func _on_dialogic_signal(argument: String) -> void:
     match argument:
         "solve_puzzle":
-            _solve_door_puzzle()
-
-func _solve_door_puzzle() -> void:
-    # Mark puzzle as solved
-    door_puzzle_solved = true
-
-    # Change RealDoor texture to tax.png
-    var tax_texture = load("res://Assets/tax.png")
-    real_door.texture = tax_texture
-
-    # Update the conversation dialogues for new state
-    _update_conversation_dialogues()
+            if not door_interacted:
+                door_interacted = true
+                ball.fall()
--- Scenes/Level1/Ball/Ball.gd
+++ Scenes/Level1/Ball/Ball.gd
@@ -4,10 +4,11 @@ extends Node2D
 @onready var shadow: Sprite2D = $Shadow
 
 func fall() -> void:
-	var tween = create_tween()
-	tween.set_parallel(true)
-	tween.set_trans(Tween.TRANS_BACK)
-	tween.set_ease(Tween.EASE_OUT)
+    var tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_OUT)
 
-	tween.tween_property(ball, "position:y", -32, 0.5)
-	tween.tween_property(shadow, "scale", Vector2.ONE, 0.5)
+    var duration = 0.5
+    tween.tween_property(ball, "position:y", -32, duration)
+    tween.tween_property(shadow, "scale", Vector2.ONE, duration)
