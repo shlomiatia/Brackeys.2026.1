7add53f06da2a2fced14c855a5a481c4ddc5dbcc
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-02-16 21:16:29 +0200

    renamed dialog

--- Levels/Level1/level1.gd
+++ Levels/Level1/level1.gd
@@ -25,14 +25,14 @@ func _show_start_sequence() -> void:
     await get_tree().create_timer(1.0).timeout
 
     # Show opening dialog
-    await DialogDisplayer.start("start_dialog")
+    await DialogDisplayer.start("level1_intro")
 
 func _update_conversation_dialogues() -> void:
     # Update Door conversation based on interaction state
     if not door_interacted:
-        door_conversation.dialogue_name = "door_locked_sound"
+        door_conversation.dialogue_name = "level1_door_loop"
     else:
-        door_conversation.dialogue_name = "door_locked"
+        door_conversation.dialogue_name = "level1_door"
 
 func _on_dialogic_signal(argument: String) -> void:
     match argument:
