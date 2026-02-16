6f625b5ceb1ee0ae7b86b858b950a7100f6854a7
amethyscent <amethyscent12@gmail.com>
2026-02-15 22:06:44 -0500

    Added in Bella's sprite sheet and image

--- Levels/node_2d_test_2.gd
+++ Levels/node_2d_test_2.gd
@@ -4,8 +4,8 @@ class_name Node2DTest2 extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-    Dialogic.signal_event.connect(_on_dialogic_signal)
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 func _on_dialogic_signal(argument: String):
-    if argument == "resumetest4":
-        animation_player.play("test4")
+	if argument == "resumetest4":
+		animation_player.play("test4")
