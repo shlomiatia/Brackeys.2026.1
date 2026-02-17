9b7fa5a9c448459b8515d025ef78c1b022262e7f
amethyscent <amethyscent12@gmail.com>
2026-02-16 19:15:13 -0500

    Adding dialogue and adjusted intro fade.

--- Levels/Level1/level1.gd
+++ Levels/Level1/level1.gd
@@ -5,34 +5,37 @@ extends Node2D
 @onready var door_conversation: Conversation = $Objects/Door/Conversation
 @onready var ball = $Objects/Ball
 @onready var knife: Sprite2D = $Knife
+@onready var fade: Fade = $CanvasLayer/Fade
 
 func _ready() -> void:
-    # Connect to Dialogic signals
-    Dialogic.signal_event.connect(_on_dialogic_signal)
+	# Connect to Dialogic signals
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 
-    # Start sequence: disable player movement and show opening dialog
-    await _show_start_sequence()
+	# Start sequence: disable player movement and show opening dialog
+	await _show_start_sequence()
 
 func _show_start_sequence() -> void:
-    Global.can_control = false
+	Global.can_control = false
 
-    # Wait 1 second
-    await get_tree().create_timer(1.0).timeout
+	# Wait 1 second
+	await get_tree().create_timer(1.0).timeout
 
-    # Show opening dialog
-    await DialogDisplayer.start("level1_intro")
+	# Show opening dialog
+	await DialogDisplayer.start("level1_intro")
 
 func _on_dialogic_signal(argument: String) -> void:
-    match argument:
-        "door_interacted":
-            ball.fall()
-        "knife_picked_up":
-            _fade_and_remove(knife)
-        "rope_cut":
-            ball.cut()
+	match argument:
+		"door_interacted":
+			ball.fall()
+		"knife_picked_up":
+			_fade_and_remove(knife)
+		"rope_cut":
+			ball.cut()
+		"show_scene":
+			fade.animation_player.play("Default")
 
 func _fade_and_remove(node: Node2D) -> void:
-    var tween = create_tween()
-    tween.tween_property(node, "modulate:a", 0.0, 0.5)
-    await tween.finished
-    node.queue_free()
+	var tween = create_tween()
+	tween.tween_property(node, "modulate:a", 0.0, 0.5)
+	await tween.finished
+	node.queue_free()
--- Scenes/Utils/fade.gd
+++ Scenes/Utils/fade.gd
@@ -3,8 +3,8 @@ class_name Fade extends ColorRect
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-    show()
+	show()
 
 func fade_out() -> void:
-    animation_player.play_backwards("Default")
-    await animation_player.animation_finished
+	animation_player.play_backwards("Default")
+	await animation_player.animation_finished
