3b64a486625ffdb3781cc14e03d3bff50858dfa0
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-02-20 12:45:50 +0200

    cp

--- Levels/Level4/maze_entrance_tile_map.gd
+++ Levels/Level4/maze_entrance_tile_map.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _ready() -> void:
 	Global.can_control = false
 	await get_tree().create_timer(1.0).timeout
-	await DialogDisplayer.start("level4_first_room")
+	await DialogDisplayer.start("enter_level4")
 	exit_interactable.interacted.connect(_on_exit_interacted)
 
 func _on_exit_interacted() -> void:
