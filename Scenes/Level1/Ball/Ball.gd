2a7af3546de3173769f7d0383f634d26c4213934
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-02-20 23:02:02 +0200

    cp

--- Scenes/Level1/Ball/Ball.gd
+++ Scenes/Level1/Ball/Ball.gd
@@ -3,47 +3,47 @@ class_name Ball extends Node2D
 @onready var ball: Sprite2D = $Ball
 @onready var shadow: Sprite2D = $Shadow
 @onready var conversation: Conversation = $Conversation
-@onready var shaking_camera: ShakingCamera = $"../../ShakingCamera"
+@onready var shaking_camera: ShakingCamera = $"../Player/ShakingCamera"
 @onready var destroy_floor: AnimatedSprite2D = $DestroyFloor
 @onready var hole: Sprite2D = $Hole
 @onready var hole_collision: CollisionShape2D = $StaticBody2D/CollisionShape2D
 
 func fall() -> void:
-	shaking_camera.start_screen_shake()
+    shaking_camera.start_screen_shake()
 
-	var tween = create_tween()
-	tween.set_parallel(true)
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.set_ease(Tween.EASE_OUT)
+    var tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_OUT)
 
-	var duration = 0.5
-	tween.tween_property(ball, "position:y", -103, duration)
-	tween.tween_property(shadow, "scale", Vector2(0.8, 0.8), duration)
+    var duration = 0.5
+    tween.tween_property(ball, "position:y", -123, duration)
+    tween.tween_property(shadow, "scale", Vector2(0.8, 0.8), duration)
 
-	await tween.finished
-	conversation.interactable_disabled = false
+    await tween.finished
+    conversation.interactable_disabled = false
 
 func cut() -> void:
-	var tween = create_tween()
-	tween.set_parallel(true)
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.set_ease(Tween.EASE_IN)
+    var tween = create_tween()
+    tween.set_parallel(true)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_IN)
 
-	var duration = 0.5
-	tween.tween_property(ball, "position:y", 0, duration)
-	tween.tween_property(shadow, "scale", Vector2.ONE, duration)
+    var duration = 0.5
+    tween.tween_property(ball, "position:y", 0, duration)
+    tween.tween_property(shadow, "scale", Vector2.ONE, duration)
 
-	await tween.finished
+    await tween.finished
 
-	shaking_camera.start_screen_shake()
+    shaking_camera.start_screen_shake()
 
-	ball.visible = false
-	shadow.visible = false
+    ball.visible = false
+    shadow.visible = false
 
-	destroy_floor.play()
+    destroy_floor.play()
 
-	hole.visible = true
-	hole_collision.disabled = false
-	z_index = 0
+    hole.visible = true
+    hole_collision.disabled = false
+    z_index = 0
 
-	conversation.dialogue_name = "level1_hole"
+    conversation.dialogue_name = "level1_hole"
