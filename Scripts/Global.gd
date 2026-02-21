a9a0b61cab1071a2bacfe128fe7489c12644f66d
amethyscent <amethyscent12@gmail.com>
2026-02-21 11:34:35 -0500

    Made lvl 6 harder, as well as some other visual changes

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -7,37 +7,37 @@ var came_from_ending: bool = false
 var incorrect_statue_count: int = 0
 
 func _unhandled_input(event: InputEvent) -> void:
-    if event.is_action_pressed("ui_cancel"):
-        Dialogic.Inputs.auto_skip.enabled = !Dialogic.Inputs.auto_skip.enabled
+	if event.is_action_pressed("ui_cancel"):
+		Dialogic.Inputs.auto_skip.enabled = !Dialogic.Inputs.auto_skip.enabled
 
 func change_scene(scene_path: String) -> void:
-    can_control = false
-    var fade: Fade = get_tree().current_scene.get_node("CanvasLayer/Fade")
-    await fade.fade_out()
-    get_tree().change_scene_to_file(scene_path)
+	can_control = false
+	var fade: Fade = get_tree().current_scene.get_node("CanvasLayer/Fade")
+	await fade.fade_out()
+	get_tree().change_scene_to_file(scene_path)
 
 func format_collected_letters() -> void:
-    var collected: String = Dialogic.VAR.get_variable("collected_letters")
-    var spaced := " ".join(collected.split(""))
-    Dialogic.VAR.set_variable("letters_display", spaced)
+	var collected: String = Dialogic.VAR.get_variable("collected_letters")
+	var spaced := " ".join(collected.split(""))
+	Dialogic.VAR.set_variable("letters_display", spaced)
 
 
 func validate_statue_answer() -> void:
-    var collected: String = Dialogic.VAR.get_variable("collected_letters")
-    var answer: String = Dialogic.VAR.get_variable("user_answer")
-    var allowed := collected.to_lower()
+	var collected: String = Dialogic.VAR.get_variable("collected_letters")
+	var answer: String = Dialogic.VAR.get_variable("user_answer")
+	var allowed := collected.to_lower()
 
-    for c in answer.to_lower():
-        if c not in allowed:
-            Dialogic.VAR.set_variable("answer_result", "missing")
-            return
+	for c in answer.to_lower():
+		if c not in allowed:
+			Dialogic.VAR.set_variable("answer_result", "missing")
+			return
 
-    if answer.to_lower() == "lies":
-        Dialogic.VAR.set_variable("answer_result", "correct")
-    elif answer.to_lower() == "self":
-        Dialogic.VAR.set_variable("answer_result", "self")
-    else:
-        Dialogic.VAR.set_variable("answer_result", "incorrect")
-        incorrect_statue_count += 1
-        if incorrect_statue_count == 3:
-            Dialogic.VAR.set_variable("bella_suspects_statues", true)
+	if answer.to_lower() == "lies":
+		Dialogic.VAR.set_variable("answer_result", "correct")
+	elif answer.to_lower() == "self":
+		Dialogic.VAR.set_variable("answer_result", "self")
+	else:
+		Dialogic.VAR.set_variable("answer_result", "incorrect")
+		incorrect_statue_count += 1
+		if incorrect_statue_count == 3:
+			Dialogic.VAR.set_variable("bella_suspects_statues", true)
